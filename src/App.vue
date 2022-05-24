a495a8ef2c3d150c06625e8ea9f24510ff0c55d0
wjddk0909 <fjddkcb@naver.com>
2022-05-24 21:54:53 +0900

    결합력 높은 차트 컴포넌트 모듈 제작 방법

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h2>Chart.js</h2>
-    <bar-chart></bar-chart>
+    <bar-chart v-bind:propsdata="chartDataSet"></bar-chart>
     <line-chart></line-chart>
   </div>
 </template>
@@ -16,6 +16,36 @@ export default {
   components: {
     BarChart,
     LineChart,
+  },
+  data() {
+    return {
+      chartDataSet: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }],
+    }
+  },
+  created() {
+    // axios.get('charts/line/1')
+    //     .then(response => this.ChartDataSet = response.data)
+    //     .catch(error => console.log(error));
   }
 }
 </script>
